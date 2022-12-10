b27656564b1b93a91657b47ff4ae9b5035b1ee56
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-12-11 01:52:43 +0530

    Fixed footer positioning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-gray-900">
+    <div className="mt-auto bg-gray-900">
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="grid row-gap-10 mb-8 lg:grid-cols-6">
           <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
