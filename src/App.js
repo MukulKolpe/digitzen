dcec48f38dd0259c9a09244913465ac608042ce1
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-12-10 16:44:55 +0530

    Configured tailwindcss + Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import UploadFile from "./pages/UploadFile/UploadFile";
 import ViewFile from "./pages/ViewFile/ViewFile";
+import { Nav } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/upload" element={<UploadFile />} />
